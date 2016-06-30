fe8ccf922ca91c689f93a543b4cd2c295ea3a6ad
Avra Neel(aka. MaBunny) <Find me on irc.freenode.net>
2016-06-30 19:04:28 +0530

    Included the changed compile script.

--- compile-without-flags.sh
+++ compile-without-flags.sh
@@ -2,7 +2,7 @@
 
 echo "Compiling Config class...."
 
-g++ -std=c++11 -fmessage-length=10 -fdiagnostics-color=always -Wall -Wextra -pedantic -L. -lstdc++ -c ./Src/config.cpp
+g++ -std=c++11 -fmessage-length=100 -fdiagnostics-color=always -Wall -Wextra -pedantic -L. -lstdc++ -c ./Src/config.cpp
 
 echo "Done compiling Config class,compiling main now...."
 
